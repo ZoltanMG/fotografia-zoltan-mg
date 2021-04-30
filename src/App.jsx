eab85c68ff5647753e789ca9481b215e1f038d0f
ZoltanMG <zoltanmoragarcia@gmail.com>
2021-04-30 17:35:26 -0500

    [add]: se crearon las rutas inicio, galeria y contacto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React from 'react';
+import Galeria from './components/galeria/Galeria'
+import Contacto from './components/contacto/Contacto'
+import Inicio from './components/inicio/Inicio'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Inicio</Link>
+          </li>
+          <li>
+            <Link to="/galeria">Galeria</Link>
+          </li>
+          <li>
+            <Link to="/contacto">Contacto</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/" component={Inicio} />
+        <Route exact path="/galeria" component={Galeria} />
+        <Route exact path="/contacto" component={Contacto} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/galeria/Galeria.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Galeria = () => {
+    return (
+        <div>
+            <h1>galeria</h1>
+        </div>
+    )
+}
+
+export default Galeria
