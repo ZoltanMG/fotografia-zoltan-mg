eaef120f7d4f4a13bff087e540c2a05d59ff73e0
ZoltanMG <zoltanmoragarcia@gmail.com>
2021-04-30 19:00:46 -0500

    [update]: se personalizo parcialmente el menu

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './nav.css'
 
 const Nav = () => {
     return (
         <nav>
-            <Link className="logo" to="/" >Zoltán MG</Link>
-            <ul>
-                <li>
-                    <Link className="links" to="/">Inicio</Link>
-                </li>
-                <li>
-                    <Link className="links" to="/galeria">Galeria</Link>
-                </li>
-                <li>
-                    <Link className="links" to="/contacto">Contacto</Link>
-                </li>
-            </ul>
+            <div className="navbar">
+                <Link className="logo" to="/" >Zoltán MG</Link>
+                <ul>
+                    <li>
+                        <Link className="links" to="/">Inicio</Link>
+                    </li>
+                    <li>
+                        <Link className="links" to="/galeria">Galeria</Link>
+                    </li>
+                    <li>
+                        <Link className="links" to="/contacto">Contacto</Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
