9da2bacc8a2f1d3401e2f12e9bef65c30e91c1c7
ZoltanMG <zoltanmoragarcia@gmail.com>
2021-04-30 19:01:34 -0500

    [add]: en el componente galeria se dividio en 2, galerias y todas las fotos

--- src/components/galeria/Galeria.jsx
+++ src/components/galeria/Galeria.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './galeria.css'
 
 const Galeria = () => {
+
+    const [galeriaOFoto, setGaleriaOFoto] = useState('galeria')
+
+    const galeria = () => {
+        setGaleriaOFoto('galeria')
+    }
+    const fotos = () => {
+        setGaleriaOFoto('fotos')
+    }
     return (
-        <div>
-            <h1>galeria</h1>
+        <div className="pagina-galeria">
+            <div className="contenedor-galeria">
+                <div className="botones-galeria">
+                    <button onClick={galeria}>Galerias</button>
+                    <button onClick={fotos}>Todas las fotos</button>
+                </div>
+                <div className="contenido-galeria">
+                    {
+                        galeriaOFoto === 'galeria' ?
+                            (
+                                <div>Galerias</div>
+                            )
+                            :
+                            (
+                                <div>Fotos</div>
+                            )
+                    }
+                </div>
+            </div>
+
         </div>
     )
 }
